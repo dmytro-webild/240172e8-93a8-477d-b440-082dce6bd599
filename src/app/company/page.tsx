a9f22bbd0f260805a3f088cf47950290f8c0bff9
bender <kudin.dima@gmail.com>
2026-05-12 18:50:34 +0000

    Update src/app/company/page.tsx

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { ThemeProvider } from "@/providers/themeProvider/ThemeProvider";
 import ReactLenis from "lenis/react";
 import NavbarStyleCentered from '@/components/navbar/NavbarStyleCentered/NavbarStyleCentered';
@@ -8,11 +7,17 @@ import TextAbout from '@/components/sections/about/TextAbout';
 
 export default function CompanyPage() {
   return (
-    <ThemeProvider>
+    <ThemeProvider defaultButtonVariant="text-stagger" defaultTextAnimation="entrance-slide" borderRadius="rounded" contentWidth="medium" sizing="medium" background="circleGradient" cardStyle="glass-elevated" primaryButtonStyle="gradient" secondaryButtonStyle="glass" headingFontWeight="normal">
       <ReactLenis root>
-        <NavbarStyleCentered navItems={[{ name: "Home", id: "/" }, { name: "About", id: "/about" }, { name: "Company", id: "/company" }]} brandName="The Guerini Group" />
-        <TextAbout title="Our Company" />
-        <FooterBaseReveal logoText="The Guerini Group" columns={[{ title: "Company", items: [{ label: "Home", href: "/" }] }]} />
+        <div id="nav" data-section="nav">
+          <NavbarStyleCentered navItems={[{ name: "Home", id: "/" }, { name: "About", id: "/about" }, { name: "Company", id: "/company" }]} />
+        </div>
+        <div id="about" data-section="about">
+          <TextAbout title="Our Company" useInvertedBackground={false} />
+        </div>
+        <div id="footer" data-section="footer">
+          <FooterBaseReveal logoText="The Guerini Group" columns={[{ title: "Company", items: [{ label: "Home", href: "/" }] }]} />
+        </div>
       </ReactLenis>
     </ThemeProvider>
   );
